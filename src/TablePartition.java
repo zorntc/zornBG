1e3720327e8eb8b6aabec7d5e3e72fd8bb2a5f53
Zorn Hsu <zorntc+git@gmail.com>
2013-04-14 19:03:44 -0700

    Secondar index added.

--- src/TablePartition.java
+++ src/TablePartition.java
@@ -2,45 +2,74 @@ import java.util.*;
 
 public class TablePartition {
 
-	static int[] workloadCnt = {8,7,1,1,1,9,1,1,1,1};
+	static int[] workloadCnt = {8,7,100,1,1,9,1,1,1,1,1,1};
 	static String[] actionName = {"AcceptFriend", "AcceptFriend", "AcceptFriend", "AcceptFriend", "AcceptFriend", "AcceptFriend", "AcceptFriend", "AcceptFriend", "AcceptFriend", "AcceptFriend"};
-	static boolean[] readOnly = {true, true, true, false, true, true, true, false, false, true};
-	static String[] whereClause = {"inviteeID","inviterID","inviteeID","inviteeID","reqID","reqID","reqID","reqID","1","1"};
-	static String[] table = {"pendfriendship", "pendfriendship", "friendship", "friendship", "friendship", "friendship", "friendship", "friendship", "qwer", "qwer"};
+	static boolean[] readOnly = {true, true, false, true, false, true, true, true, true, false, true, true};
+	static String[] column = {"inviteeID","inviteeID","inviterID","inviterID","inviteeID","inviteeID","inviteeID","inviterID","inviteeID","inviteeID","userID","userID"};
+	static String[] table = {"pendfriendship", "pendfriendship", "pendfriendship", "pendfriendship", "conffriendship", "conffriendship", "conffriendship", "conffriendship", "conffriendship", "conffriendship", "User", "User"};
 
-	static LinkedList<Table> tableList = new LinkedList<Table>();
+	static LinkedList<Table> partitionList = new LinkedList<Table>();
+	static LinkedList<Table> replicationList = new LinkedList<Table>();
 
 	static void partition(){
 		int i;
 		Table ta = null;
-		for(i = 0; i < whereClause.length; i++){
+		for(i = 0; i < column.length; i++){
 			ta = null;
-			for(Table t : tableList){
+			for(Table t : partitionList){
 				if(!t.name.equals(table[i]))
 					continue;
 				ta = t;
 				break;
 			}
 			if(ta == null){						 
-				tableList.add(new Table(table[i]));
-				ta = tableList.getLast();
+				partitionList.add(new Table(table[i]));
+				ta = partitionList.getLast();
 			}
-			ta.inc(whereClause[i], workloadCnt[i]);
+			ta.inc(column[i], workloadCnt[i]);
+			if(!readOnly[i]){
+				ta.replication = false;
+				ta.modifiedCol.add(column[i]);
+			}
+		}
+	}
+
+	static void replication(){
+		ListIterator<Table> ite = partitionList.listIterator(0);
+		Table t;
+		while(ite.hasNext()){
+			t = ite.next();
+			if(t.replication)
+				replicationList.add(t);
+		}
+
+		ite = replicationList.listIterator(0);
+		while(ite.hasNext()){
+			t = ite.next();
+			partitionList.remove(t);
 		}
 	}
 
 	public static void main(String[] args){
 		partition();
-		for(Table tl: tableList){
-			System.out.printf("%s: partition attr is %s\n", tl.name, tl.getPartAttr());
+		replication();
+
+		for(Table tl: partitionList){
+			System.out.printf("partition %s by %s, second index: %s\n", tl.name, tl.getPartAttr(), tl.getSecIndex());
+		}
+		for(Table tl: replicationList){
+			System.out.printf("replicate %s\n", tl.name);
 		}
 	}
 }
 
 class Table{
 	String name = null;
-	String partitionAttr = null;
+	private String partitionAttr = null;
+	private String secIndex = null;
+	boolean replication = true;
 	TreeMap<String, Integer> attr = new TreeMap<String, Integer>();
+	HashSet<String> modifiedCol = new HashSet<String>();
 
 	public boolean equals(String s){
 		return (s.equalsIgnoreCase(name));
@@ -60,17 +89,42 @@ class Table{
 		}
 	}
 
-	public String getPartAttr(){
-		if(partitionAttr != null) 
-			return partitionAttr;
+	public void computePartAttrSecIndex(){
 		Map.Entry<String, Integer> me;
-		int cnt = 0;
+		int max = 0, maxMax = 0;
 		while((me = attr.pollFirstEntry())!= null){
-			if(me.getValue() <= cnt)
+			if(me.getValue() < max)
 				continue;
-			cnt = me.getValue();
-			partitionAttr = me.getKey();
+			if(maxMax <= me.getValue()){
+				if(!modifiedCol.contains(partitionAttr)){
+					max = maxMax;
+					secIndex = partitionAttr;
+				}
+				maxMax = me.getValue();
+				partitionAttr = me.getKey();
+			}
+			else{ 	// max <= me < maxMax
+				if(!modifiedCol.contains(me.getKey()))
+					continue;
+				max = me.getValue();
+				secIndex = me.getKey();
+			}
 		}
+		if(partitionAttr == null)
+			partitionAttr = "NIL";
+		if(secIndex == null)
+			secIndex = "NIL";
+	}
+
+	public String getPartAttr(){
+		if(partitionAttr == null)
+			computePartAttrSecIndex();
 		return partitionAttr;
+	}
+
+	public String getSecIndex(){
+		if(secIndex == null)
+			computePartAttrSecIndex();
+		return secIndex;
 	}
 }
